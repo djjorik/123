23c0eac05e0c07f4a1407f387234fab0a2d0a1e7
djjorik <shoopikbat@gmail.com>
2017-08-02 17:32:30 +0500

    123

---
+++ app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+//import {HttpClientModule, HttpClient} from '@angular/common/http';
+import { AppComponent }   from './app.component';
+@NgModule({
+    imports:      [ BrowserModule, FormsModule ],
+    declarations: [ AppComponent ],
+    bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
